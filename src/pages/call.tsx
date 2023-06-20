2abd3cfe57796f163ebd1c110864daf002a50c28
ZzRanger <calebmarklim@gmail.com>
2023-06-19 21:50:10 -0500

    refactor page to be client side rendered

--- src/pages/call.tsx
+++ src/pages/call.tsx
@@ -1,23 +1,35 @@
-import { GetServerSideProps } from 'next';
-
 import { Call, Device } from '@twilio/voice-sdk';
 import { useEffect, useRef, useState } from 'react';
 
-export default function CallPage({ test }) {
+export default function CallPage() {
   const device = useRef<any>(null);
   const [call, setCall] = useState(null);
   const [number, setNumber] = useState(null);
+  const [deviceLoaded, setDeviceLoaded] = useState(false);
+  const [identity, setIdentity] = useState('');
 
   const [incomingCall, setIncomingCall] = useState(false);
 
-  useEffect(() => {
-    device.current = new Device(test.token, {
+  async function initializeDevice() {
+    const test = await fetch('http://localhost:3000/api/token');
+
+    const value = await test.json();
+
+    setIdentity(value.identity);
+
+    device.current = new Device(value.token, {
       logLevel: 1,
       codecPreferences: [Call.Codec.Opus, Call.Codec.PCMU],
     });
 
     addDeviceListeners(device.current);
-    device.current.register();
+    await device.current.register();
+
+    setDeviceLoaded(true);
+  }
+
+  useEffect(() => {
+    initializeDevice();
   });
 
   function addDeviceListeners(device) {
@@ -58,40 +70,30 @@ export default function CallPage({ test }) {
       call.on('accept', () => alert('ACCEPT'));
       call.on('disconnect', () => alert('DISCONNECT'));
       call.on('cancel', () => alert('CANCEL'));
-
-      // outgoingCallHangupButton.onclick = () => {
-      //   log('Hanging up ...');
-      //   call.disconnect();
-      // };
     }
   };
+
+  if (!deviceLoaded) {
+    return <div>Loading</div>;
+  }
   return (
     <main className="w-screen h-screen flex flex-col">
-      <h1 className="text-3xl">Welcome back {test.identity}</h1>
-      <section className="flex flex-col gap-y-4 bg-gray-100 h-80 w-80 p-4">
-        <h2 className="text-xl">Make Outgoing Call</h2>
-        <input
-          onChange={(e) => setNumber(e.target.value)}
-          value={number}
-          placeholder="Enter number here"
-          className="w-40"
-        />
-        <button className="w-20 bg-gray-300 " onClick={makeCall}>
-          Call
-        </button>
-      </section>
-      {incomingCall && <button onClick={acceptCall}>Accept Call</button>}{' '}
-      <div>Test</div>
+      <h1 className="text-3xl">Welcome back {identity}</h1>
+      <div>
+        <section className="flex flex-col gap-y-4 bg-gray-100 h-80 w-80 p-4">
+          <h2 className="text-xl">Make Outgoing Call</h2>
+          <input
+            onChange={(e) => setNumber(e.target.value)}
+            value={number}
+            placeholder="Enter number here"
+            className="w-40"
+          />
+          <button className="w-20 bg-gray-300 " onClick={makeCall}>
+            Call
+          </button>
+          {incomingCall && <button onClick={acceptCall}>Accept Call</button>}
+        </section>
+      </div>
     </main>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const test = await fetch('http://localhost:3000/api/token');
-
-  const value = await test.json();
-
-  return {
-    props: { test: value },
-  };
-};
