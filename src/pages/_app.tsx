588474577cda4be6fbb4653e599688ddf95d62c4
elliecopps <elliecopps11@gmail.com>
2023-07-05 10:44:34 -0500

    savign progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Input } from 'postcss'
+import { useState } from 'react';
+import { putData, deleteData } from '../add_data'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  async function handleAddEntry () {
+    try{
+      await( putData('api/dynamoCreate', {TableName: "starter_db", Item: {
+        org_id: 'org-Id',
+        user_id: 'user-Id'
+      }}))
+    }catch{
+      console.log("error putting data!")
+    }
+  }
+  
+  async function handleDeleteEntry () {
+    try{
+      await( deleteData('api/dynamoDelete', {TableName: "starter_db", Key: {
+        org_id: 'org-Id',
+        user_id: 'user-Id'
+      }}))
+    }catch{
+      console.log("error putting data!")
+    }
+  }
+
+  return <div>
+    
+    <button onClick={handleAddEntry}>Add entry!</button>
+    <button onClick={handleDeleteEntry}>Delete entry!</button>
+    <Component {...pageProps} />
+  </div>
+    
 }
