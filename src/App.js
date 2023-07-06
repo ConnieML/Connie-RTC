2f24c6d58df5d947ec2972e07b1be39f0969c755
jinl33 <lee3598@purdue.edu>
2023-07-06 23:26:40 +0900

    cbo profile

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CBOProfilePage from './CBOProfilePage';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <CBOProfilePage />
+    </div>
+  );
+};
+
+export default App;
