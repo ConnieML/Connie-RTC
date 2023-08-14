62daa36d5b45c3dffb4117257c39d8a7154a2bbd
ZzRanger <calebmarklim@gmail.com>
2023-08-13 22:06:12 -0500

    quick edits

--- src/pages/logging.tsx
+++ src/pages/logging.tsx
@@ -34,19 +34,20 @@ export default function Line() {
         // Define the start time and end time values
         const startTime = '2023-06-10';
         const endTime = '2023-06-20';
-  
+
         // Append the start time and end time values to the URL as query parameters
-        const response = await fetch(`/api/calls?startTime=${startTime}&endTime=${endTime}`);
+        const response = await fetch(
+          `/api/calls?startTime=${startTime}&endTime=${endTime}`
+        );
         const data = await response.json();
         setCallData(data);
       } catch (error) {
         console.log('Error fetching data:', error);
       }
     };
-  
+
     fetchData();
   }, []);
-  
 
   const transformData = (data: CallData[]): number[] => {
     let callsByInterval: number[] = [];
@@ -102,7 +103,10 @@ export default function Line() {
     return durationsByInterval;
   };
 
-  const calculateAverageDurations = (callData: CallData[], durationsByInterval: number[]): number[] => {
+  const calculateAverageDurations = (
+    callData: CallData[],
+    durationsByInterval: number[]
+  ): number[] => {
     const callCounts = transformData(callData);
     return durationsByInterval.map((totalDuration, index) =>
       callCounts[index] > 0 ? totalDuration / callCounts[index] : 0
@@ -111,31 +115,57 @@ export default function Line() {
 
   const transformedData: number[] = transformData(callData);
   const transformedDurations: number[] = transformDurationData(callData);
-  const avgDurationsByInterval: number[] = calculateAverageDurations(callData, transformedDurations);
+  const avgDurationsByInterval: number[] = calculateAverageDurations(
+    callData,
+    transformedDurations
+  );
 
   useEffect(() => {
     if (callChartDataRef.current === null) return;
-    const ctx = callChartDataRef.current.getContext('2d') as CanvasRenderingContext2D;
+    const ctx = callChartDataRef.current.getContext(
+      '2d'
+    ) as CanvasRenderingContext2D;
 
     let labels: string[] = [];
     let data: number[] = [];
-    let yAxesMax = 0;
+    let yAxesMax = 1.25 * Math.max(...transformedData);
 
     if (filterBy === 'day') {
-      yAxesMax = 2500; // Maximum value on y-axes for 'day' filter
       labels = [
-        '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00',
-        '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
-        '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+        '00:00',
+        '01:00',
+        '02:00',
+        '03:00',
+        '04:00',
+        '05:00',
+        '06:00',
+        '07:00',
+        '08:00',
+        '09:00',
+        '10:00',
+        '11:00',
+        '12:00',
+        '13:00',
+        '14:00',
+        '15:00',
+        '16:00',
+        '17:00',
+        '18:00',
+        '19:00',
+        '20:00',
+        '21:00',
+        '22:00',
+        '23:00',
       ];
       data = transformedData;
     } else if (filterBy === 'week') {
-      yAxesMax = 1.25  // Maximum value on y-axes for 'week' filter
       labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
       data = transformedData;
     } else if (filterBy === 'month') {
-      yAxesMax = 1.25 ; // Maximum value on y-axes for 'month' filter
-      labels = Array.from({ length: transformedData.length }, (_, i) => i + 1).map(String);
+      labels = Array.from(
+        { length: transformedData.length },
+        (_, i) => i + 1
+      ).map(String);
       data = transformedData;
     }
 
@@ -148,8 +178,8 @@ export default function Line() {
           backgroundColor: colors.purple.half,
           borderColor: colors.indigo.default,
           pointStyle: 'circle',
-          pointRadius: 6,         // Adjust the size of the circles
-          pointHoverRadius: 9,    // Adjust the size of the circles on hover
+          pointRadius: 6, // Adjust the size of the circles
+          pointHoverRadius: 9, // Adjust the size of the circles on hover
         },
       ],
     };
@@ -172,16 +202,15 @@ export default function Line() {
         },
         scales: {
           x: {
-            ticks: { color: 'white' },
+            ticks: { color: 'black' },
             grid: {
               color: () => '#969696',
             },
           },
           y: {
             ticks: {
-              color: 'white',
+              color: 'black',
               stepSize: yAxesMax / 5, // Divide the y-axes range into 5 equal steps
-              callback: (value) => (typeof value === 'number' ? (value === 0 ? '0' : `${value / 1000}k`) : value),
             },
             max: yAxesMax,
             grid: {
@@ -201,30 +230,56 @@ export default function Line() {
 
   useEffect(() => {
     if (avgDurationChartDataRef.current === null) return;
-    const ctx = avgDurationChartDataRef.current.getContext('2d') as CanvasRenderingContext2D;
+    const ctx = avgDurationChartDataRef.current.getContext(
+      '2d'
+    ) as CanvasRenderingContext2D;
 
     let labels: string[] = [];
     let data: number[] = [];
-    let yAxesMax = 0;
+
+    let yAxesMax = 1.25 * Math.max(...avgDurationsByInterval);
 
     if (filterBy === 'day') {
-      yAxesMax = 2000; // You can adjust this value based on your data
       labels = [
-        '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00',
-        '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
-        '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+        '00:00',
+        '01:00',
+        '02:00',
+        '03:00',
+        '04:00',
+        '05:00',
+        '06:00',
+        '07:00',
+        '08:00',
+        '09:00',
+        '10:00',
+        '11:00',
+        '12:00',
+        '13:00',
+        '14:00',
+        '15:00',
+        '16:00',
+        '17:00',
+        '18:00',
+        '19:00',
+        '20:00',
+        '21:00',
+        '22:00',
+        '23:00',
       ];
       data = avgDurationsByInterval;
     } else if (filterBy === 'week') {
-      yAxesMax = 5000; // You can adjust this value based on your data
       labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
       data = avgDurationsByInterval;
     } else if (filterBy === 'month') {
-      yAxesMax = 10000; // You can adjust this value based on your data
-      labels = Array.from({ length: avgDurationsByInterval.length }, (_, i) => i + 1).map(String);
+      labels = Array.from(
+        { length: avgDurationsByInterval.length },
+        (_, i) => i + 1
+      ).map(String);
       data = avgDurationsByInterval;
     }
 
+    console.log('WAIT');
+    console.log(labels);
     const chartData = {
       labels,
       datasets: [
@@ -234,8 +289,8 @@ export default function Line() {
           backgroundColor: colors.purple.half,
           borderColor: colors.indigo.default,
           pointStyle: 'circle',
-          pointRadius: 6,         // Adjust the size of the circles
-          pointHoverRadius: 9,    // Adjust the size of the circles on hover
+          pointRadius: 6, // Adjust the size of the circles
+          pointHoverRadius: 9, // Adjust the size of the circles on hover
         },
       ],
     };
@@ -259,16 +314,17 @@ export default function Line() {
         },
         scales: {
           x: {
-            ticks: { color: 'white' },
+            ticks: { color: 'black' },
             grid: {
               color: () => '#969696',
             },
           },
           y: {
             ticks: {
-              color: 'white',
+              color: 'black',
               stepSize: yAxesMax / 5, // Divide the y-axes range into 5 equal steps
-              callback: (value) => (typeof value === 'number' ? `${value}s` : value),
+              callback: (value) =>
+                typeof value === 'number' ? `${value}s` : value,
             },
             max: yAxesMax,
             grid: {
@@ -278,7 +334,6 @@ export default function Line() {
         },
       },
     };
-    
 
     const myLineChart = new Chart(ctx, config);
 
@@ -290,30 +345,32 @@ export default function Line() {
   return (
     <article>
       <div>
-      <div>
-        <span style={{ marginLeft: '3rem'}}>Filter By:</span>{' '}
-        <select
-          value={filterBy}
-          onChange={(e) => setFilterBy(e.target.value as 'day' | 'week' | 'month')}
-          style={{
-            borderRadius: '0.25rem', // Add rounded edges
-            backgroundColor: '#f3f4f6', // Change the background color
-            padding: '0.5rem', // Add some padding
-          }}
-        >
-          <option value="day">Day</option>
-          <option value="week">Week</option>
-          <option value="month">Month</option>
-        </select>
-      </div>
-      <div style={{ display: 'flex', maxWidth: '100vw', overflowX: 'auto' }}>
-        <div style={{ flex: 1 }}>
-          <canvas ref={callChartDataRef} style={{ width: '100%' }} />
+        <div>
+          <span style={{ marginLeft: '3rem' }}>Filter By:</span>{' '}
+          <select
+            value={filterBy}
+            onChange={(e) =>
+              setFilterBy(e.target.value as 'day' | 'week' | 'month')
+            }
+            style={{
+              borderRadius: '0.25rem', // Add rounded edges
+              backgroundColor: '#f3f4f6', // Change the background color
+              padding: '0.5rem', // Add some padding
+            }}
+          >
+            <option value="day">Day</option>
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+          </select>
         </div>
-        <div style={{ flex: 1 }}>
-          <canvas ref={avgDurationChartDataRef} style={{ width: '100%' }} />
+        <div style={{ display: 'flex', maxWidth: '100vw', overflowX: 'auto' }}>
+          <div style={{ flex: 1 }}>
+            <canvas ref={callChartDataRef} style={{ width: '100%' }} />
+          </div>
+          <div style={{ flex: 1 }}>
+            <canvas ref={avgDurationChartDataRef} style={{ width: '100%' }} />
+          </div>
         </div>
-      </div>
       </div>
     </article>
   );
