e44720796b9efca9d6fc788ccbce67a28fe7860b
Alex Tsarapkine <alexander.tsarapkine@gmail.com>
2024-01-13 13:56:00 -0500

    Added basic OIDC auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
-import { useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import React from 'react';
-import { redirect } from 'next/navigation'
+
 
 export default function Home() {
   const {data: session, status} = useSession();
   if (status === 'loading') {
     return <React.Fragment>Loading...</React.Fragment>;
-  } else if (session) {
-    redirect('/dashboard');
-  } else {
-    redirect('/login');
   }
+  if (session) {
+    console.log(session);
+    return (
+      <React.Fragment>
+        Welcome to the dashboard {session.user.name} - {session.userType}{' '}
+        <button onClick={() => signOut()}>Sign out</button>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={() => signIn('okta')}>Sign in with Okta</button>
+    </React.Fragment>
+  );
 }
