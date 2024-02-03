2edb15bc600cb2101e875fea48f0e6946554a53e
Alex Tsarapkine <alexander.tsarapkine@gmail.com>
2024-02-02 20:41:20 -0500

    finished auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,9 +7,49 @@ export const authOptions = {
             clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
             clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
             issuer: process.env.OKTA_OAUTH2_ISSUER as string,
+            authorization: {
+                params: {scope: 'openid profile email groups' }
+            }
         }),
     ],
-    secret: process.env.SECRET as string
-}
+    secret: process.env.SECRET as string,
+    callbacks: {
+        async jwt({ token, account }: any) {
+            if (account) {
+                
+                token.accessToken = account.access_token;
+                token.idToken = account.id_token;
+                token.oktaId = account.providerAccountId;
+                token.groups = account.groups;
+            }
+            
 
-export default NextAuth(authOptions)
+            // Decrypting JWT to check if expired
+            var tokenParsed = JSON.parse(
+                Buffer.from(token.idToken.split('.')[1], 'base64').toString()
+            );
+            const dateNowInSeconds = new Date().getTime() / 1000;
+            if (dateNowInSeconds > tokenParsed.exp) {
+                throw Error('expired token');
+            }
+
+            // Add fields to token that are useful
+            token.employeeNumber = tokenParsed.employeeNumber;
+            token.groups = tokenParsed.groups;
+
+            return token;
+        },
+        async session({ session, token }: any) {
+            session.accessToken = token.accessToken;
+            session.idToken = token.idToken;
+            session.oktaId = token.oktaId;
+            session.userType = token.userType;
+            session.employeeNumber = token.employeeNumber;
+            session.groups = token.groups;
+
+            return session;
+        },
+    },
+};
+
+export default NextAuth(authOptions);
