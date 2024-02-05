22be425416062a34f573085d8f3c08ed84334ad8
Paul Loh <lohpaul9@gmail.com>
2024-02-05 16:30:00 -0500

    fetching data tested

---
+++ src/lib/crm_data/types.ts
@@ -0,0 +1,13 @@
+// Data type that encapsulates 
+export type CRMEntry = { [key: string]: string };
+
+export interface CRMTable {
+  getRowById(id: string): Promise<CRMEntry | null>;
+  getClientByPhone(phoneNumber: string): Promise<CRMEntry | null>;
+  storeClientByPhone(phoneNumber: string, client: CRMEntry): Promise<void>;
+}
+
+export interface ConnieCRMProvider {
+  getTable(id: string): CRMTable;
+  getDefaultTable(): CRMTable;
+}
