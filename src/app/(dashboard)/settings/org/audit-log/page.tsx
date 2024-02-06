7f5a61481ba9cdeb9df40b97eb133717c12457a1
minfei <minfei@sas.upenn.edu>
2024-02-05 23:38:23 -0500

    UI changes

--- src/app/(dashboard)/settings/org/audit-log/page.tsx
+++ src/app/(dashboard)/settings/org/audit-log/page.tsx
@@ -6,7 +6,11 @@ export default function AuditLog() {
 
     return (
       <main>
-        <h1>AuditLog Page is here</h1>
+      <h1 className="text-3xl font-bold">Audit Log</h1>
+      <p className="text-[#64748B] mt-2 text-sm">
+        View all calls and SMS message your organization made via Connie.
+      </p>
+      <hr className="my-6 border-t border-gray-200" />
         <div>
             <AuditTable/>
         </div>
