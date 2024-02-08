681f9eec3e52541ae8881d53364de471aa9e4b4f
michelle <michelleshx462@gmail.com>
2024-02-08 16:19:03 -0500

    client page ui

--- src/app/dashboard/clients/page.tsx
+++ src/app/dashboard/clients/page.tsx
@@ -1,7 +1,50 @@
-export default function Clients() {
+import { ClientTable } from "@/components/clientTable/ClientTable";
+import { columns, Client } from "@/components/clientTable/columns";
+
+async function getData(): Promise<Client[]> {
+  // TODO get data from airtable
+  return [
+    {
+      id: "617394",
+      client: "Judy S.",
+      phoneNumber: "+12345678901",
+      email: "judys@gmail.com",
+      lastContacted: "January 28, 2024, 5:26pm",
+    },
+    {
+      id: "617394",
+      client: "Judy S.",
+      phoneNumber: "+12345678901",
+      email: "judys@gmail.com",
+      lastContacted: "January 28, 2024, 5:26pm",
+    },
+    {
+      id: "617394",
+      client: "ABC",
+      phoneNumber: "+12345678901",
+      email: "abc@gmail.com",
+      lastContacted: "January 28, 2024, 5:26pm",
+    },
+    {
+      id: "617394",
+      client: "Judy S.",
+      phoneNumber: "+12345678901",
+      email: "judys@gmail.com",
+      lastContacted: "January 28, 2024, 5:26pm",
+    },
+  ];
+}
+
+export default async function Clients() {
+  const data = await getData();
+
   return (
-    <main className="flex flex-row justify-between">
-      <div> All clients</div>
+    <main className="p-4">
+      <h1 className="text-3xl font-bold">Clients</h1>
+      <p className="mt-3 font-semibold pb-2">
+        Browse and search for all client contacts here.
+      </p>
+      <ClientTable columns={columns} data={data} />
     </main>
   );
 }
