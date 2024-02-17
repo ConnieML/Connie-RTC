4dd90130ea078b89ae1b6467f3207367e9c21c1f
lohpaul9 <paull@shoreline.io>
2024-02-17 14:42:11 -0500

    Upload token to S3 complete

--- src/app/api/crmconfig/airtable/route.ts
+++ src/app/api/crmconfig/airtable/route.ts
@@ -1,22 +1,36 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from "next-auth/next";
-import { putObject } from '../../../../lib/aws/s3';
-import { authOptions } from '../../../../pages/api/auth/[...nextauth]';
+import { putObject, getObject } from '../../../../lib/aws/s3';
+// import { authOptions } from '../../../../pages/api/auth/[...nextauth]';
+import { authOptions } from '@/lib/auth';
+import { NextRequest, NextResponse } from 'next/server';
 
+type AirtableTokenData = {
+  token: string;
+};
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(req, res, authOptions);
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions) as any;
+  
+  
+  const res = NextResponse.next();
   if (!session) {
-    res.status(401).send('Unauthorized');
-    return;
+    return new Response('Unauthorized', { status: 401 });
   }
 
+  const oktaId  = session.oktaId;
+  const body = await req.json() as AirtableTokenData;
+  const token = body.token;
+
   try {
-    const { body } = req.body;
-    const key = `crm_airtable:${session.user.id}`;
-    await putObject(key, body);
-    res.status(200).send('Success');
+    const key = `crm_airtable:${oktaId}`;
+    await putObject(key, token);
+    // sanity check to ensure it stored
+    const storedToken = await getObject(key);
+    console.log("Stored token: ", storedToken);
+    return new Response('Success', { status: 200 });
   } catch (error) {
-    res.status(500).send('Error');
+    console.error(error);
+    return new Response('Put in S3 unsuccessful: ' + error, { status: 500 });
   }
 };
