a208d2007e61a261a79d102e2bcacddc4b894f7b
lohpaul9 <paull@shoreline.io>
2024-02-17 14:42:11 -0500

    Upload token to S3 complete

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React from 'react';
+import SingleForm from '@/components/ui/single-form';
+
+const Page = () => {
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const field = formData.get('Airtable Token');
+        console.log(field)
+
+
+        try {
+            const response = await fetch('../api/crmconfig/airtable', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ token: field }),
+            });
+            // Handle the response
+        } catch (error) {
+            // Handle the error
+        }
+    };
+
+    return (
+        <div>
+            <h1>Page</h1>
+            <SingleForm onSubmit={onSubmit} formField="Airtable Token" formLabel="Airtable Token"/>
+        </div>
+    );
+};
+
+export default Page;
