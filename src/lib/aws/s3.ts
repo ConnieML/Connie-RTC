8d1ad755bdec5985068377b2a2bc607f488ef084
lohpaul9 <paull@shoreline.io>
2024-02-18 22:11:59 -0500

    completed single form s3 integration

--- src/lib/aws/s3.ts
+++ src/lib/aws/s3.ts
@@ -18,13 +18,19 @@ export const putObject = async (key: string, body: string) => {
   await s3.send(command);
 };
 
-export const getObject = async (key: string) => {
+export const getObjectString = async (key: string) => {
   const params = {
     Bucket: bucket_name,
     Key: key,
   };
   const command = new GetObjectCommand(params);
   const { Body } = await s3.send(command);
-  return Readable.from(Body).toString();
+  const stream = Body as Readable;
+  // convert the stream into a string
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks).toString('utf8');
 };
 
