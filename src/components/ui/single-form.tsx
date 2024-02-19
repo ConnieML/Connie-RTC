9666fb4c8634d7d6946f10bf9b6c0e9364002c64
lohpaul9 <paull@shoreline.io>
2024-02-18 23:59:02 -0500

    complete basic CRM configuration

--- src/components/ui/single-form.tsx
+++ src/components/ui/single-form.tsx
@@ -1,30 +1,28 @@
 'use client';
 
+
 import React from 'react';
 import * as Form from '@radix-ui/react-form';
 
-/**
- * Should take in the name of the field
- * As well as the callback to submission
- */
-
 interface SingleFormProps {
     onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-    formField: string;
-    formLabel: string;
+    fields: { name: string; label: string }[];
 }
 
-const SingleForm = ({ onSubmit, formField, formLabel }: SingleFormProps) => {
+const SingleForm = ({ onSubmit, fields }: SingleFormProps) => {
     return (
         <Form.Root onSubmit={onSubmit}>
-            <Form.Field name={formField}>
-                <Form.Label>{formLabel}</Form.Label>
-                <Form.Control type="text" required />
-
-            </Form.Field>
+            {fields.map((field) => (
+                <Form.Field key={field.name} name={field.name}>
+                    <Form.Label>{field.label}</Form.Label>
+                    <Form.Control type="text" required />
+                </Form.Field>
+            ))}
             <Form.Submit>Submit</Form.Submit>
         </Form.Root>
     );
 };
 
 export default SingleForm;
+
+export type { SingleFormProps };
