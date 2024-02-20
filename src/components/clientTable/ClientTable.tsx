ee5d37ed36c22b651c341d0081d5d582cf5747f0
michelle <michelleshx462@gmail.com>
2024-02-19 21:27:04 -0500

    fetch data from airtable

--- src/components/clientTable/ClientTable.tsx
+++ src/components/clientTable/ClientTable.tsx
@@ -8,6 +8,7 @@ import {
   getCoreRowModel,
   getFilteredRowModel,
   useReactTable,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
 import {
   Table,
@@ -17,17 +18,18 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import { Client } from "@/components/clientTable/columns";
 
 import { copyText } from "@/lib/utils";
 import useCalls from "@/lib/hooks/useCalls";
+import { CRMEntry } from "@/lib/crm/types";
 
 interface DataTableProps {
-  columns: ColumnDef<Client>[];
-  data: Client[];
+  columns: ColumnDef<CRMEntry>[];
+  data: CRMEntry[];
 }
 
 export function ClientTable({ columns, data }: DataTableProps) {
@@ -44,6 +46,7 @@ export function ClientTable({ columns, data }: DataTableProps) {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
     onGlobalFilterChange: setGlobalFilter,
     getFilteredRowModel: getFilteredRowModel(),
     state: {
@@ -60,71 +63,91 @@ export function ClientTable({ columns, data }: DataTableProps) {
   });
 
   return (
-    <div className="grid gap-6">
-      <div className="grid gap-2 pt-4">
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label>Search</Label>
-          <Input
-            id="clientSearch"
-            placeholder="Name, phone number, email"
-            value={globalFilter ?? ""}
-            onChange={(e) => setGlobalFilter(String(e.target.value))}
-          />
+    <div>
+      <div className="grid gap-6">
+        <div className="grid gap-2 pt-4">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label>Search</Label>
+            <Input
+              id="clientSearch"
+              placeholder="Name, phone number, email"
+              value={globalFilter ?? ""}
+              onChange={(e) => setGlobalFilter(String(e.target.value))}
+            />
+          </div>
+          <p className="italic text-secondary-foreground text-sm">
+            {data.length} clients synced from CRM
+          </p>
+          <Separator />
         </div>
-        <p className="italic text-secondary-foreground text-sm">
-          {data.length} clients synced from CRM
-        </p>
-        <Separator />
-      </div>
-      <div className="rounded-md border bg-white">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+        <div className="rounded-md border bg-white">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
       </div>
     </div>
   );
--- src/lib/data/clients.ts
+++ src/lib/data/clients.ts
@@ -1,80 +1,40 @@
-import { Client } from "@/components/clientTable/columns";
-import { getServerSession } from "next-auth/next";
+import {
+  AirtableCRMProvider,
+  s3KeyForAirtableToken,
+  s3KeyForAirtableBase,
+  s3KeyForAirtableTable,
+} from "../crm/airtable";
+import { getObjectString } from "@/lib/aws/s3";
 import { authOptions } from "@/lib/auth";
-import { AirtableCRMProvider } from "../crm/airtable";
-
-// ENV variables for now, pending CRM configuration feature
-const AIRTABLE_TEST_API_KEY = process.env.AIRTABLE_TEST_API_KEY || "";
-const AIRTABLE_TEST_BASE_ID = process.env.AIRTABLE_TEST_BASE_ID || "";
-const AIRTABLE_TEST_TABLE_ID = process.env.AIRTABLE_TEST_TABLE_ID || "";
-
-// TODO replace with airtable data
-const test_data = [
-  {
-    id: "12345",
-    client: "Lola M.",
-    phoneNumber: "+1222537890",
-    email: "judys@gmail.com",
-    lastContacted: "January 28, 2024, 5:26pm",
-  },
-  {
-    id: "163936",
-    client: "Judy S.",
-    phoneNumber: "+1234567890",
-    email: "judys@gmail.com",
-    lastContacted: "January 28, 2024, 5:26pm",
-  },
-  {
-    id: "4567",
-    client: "ABC",
-    phoneNumber: "+0987654321",
-    email: "abc@gmail.com",
-    lastContacted: "January 28, 2024, 5:26pm",
-  },
-  {
-    id: "192745",
-    client: "Judy S.",
-    phoneNumber: "+1234567890",
-    email: "john@gmail.com",
-    lastContacted: "January 28, 2024, 5:26pm",
-  },
-  {
-    id: "192745",
-    client: "Judy S.",
-    phoneNumber: "+1674567890",
-    email: "jane@gmail.com",
-    lastContacted: "January 28, 2024, 5:26pm",
-  },
-  {
-    id: "192745",
-    client: "Erin S.",
-    phoneNumber: "+1094567890",
-    email: "erin@gmail.com",
-    lastContacted: "January 28, 2024, 5:26pm",
-  },
-];
+import { getServerSession } from "next-auth/next";
 
 // Fetch clients data from crm
 export default async function fetchClients() {
+  const session = (await getServerSession(authOptions)) as any;
+
+  if (!session) {
+    console.error("Unauthorized");
+    return [];
+  }
+
   try {
-    // TODO uncomment when getAllRows implemented for airtable api
-    // const provider = new AirtableCRMProvider(
-    //   AIRTABLE_TEST_BASE_ID,
-    //   AIRTABLE_TEST_API_KEY
-    // );
+    const oktaId = session.oktaId;
 
-    // const table = provider.getTable(AIRTABLE_TEST_TABLE_ID);
-    // const clients = await table.getAllRows();
-    // return clients;
+    const [airtable_token, airtable_base_id, airtable_table_id] =
+      await Promise.all([
+        getObjectString(s3KeyForAirtableToken(oktaId)),
+        getObjectString(s3KeyForAirtableBase(oktaId)),
+        getObjectString(s3KeyForAirtableTable(oktaId)),
+      ]);
 
-    const mergedData: any = [];
-    test_data.forEach((obj) => {
-      const existingObj = mergedData.find(
-        (item: any) => item.phoneNumber === obj.phoneNumber
-      );
-      !existingObj && mergedData.push(obj);
+    const provider = new AirtableCRMProvider(airtable_base_id, airtable_token);
+    const table = provider.getTable(airtable_table_id);
+    const allClients = await table.getAllRows();
+
+    allClients.forEach((client) => {
+      client["full_name"] = `${client.first_name} ${client.last_name}`;
     });
-    return mergedData;
+    return allClients;
   } catch (e) {
     console.error(e);
     return [];
