5c42b7dfab11368288b327909374bf668a14fa0f
Willie Chalmers III <willie@williecubed.me>
2024-02-21 14:58:09 -0800

    Revamp landing page
    Also adopt new site-wide font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import React from 'react';
 
+import connieLogo from '@/assets/connie-logo.png';
+import asaLogo from '@/assets/asa-logo.png';
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+
+/**
+ * The landing page for Connie 
+ *
+ * Route: /
+ */
 export default function Home() {
   const { data: session, status } = useSession();
-  if (status === 'loading') {
-    return <React.Fragment>Loading...</React.Fragment>;
-  } else if (session) {
+  if (session) {
     redirect('/dashboard');
   } else {
-    redirect('/login');
+    return (
+      <div className="h-full flex flex-col justify-center">
+        <main className="h-[600px] flex flex-col items-center justify-between">
+          <Card className="mt-[10vh] p-6 max-w-md w-full text-center space-y-6">
+            <div className="my-6 flex justify-center">
+              <Image src={connieLogo} alt="" width={240} height={68} className="object-contain" />
+            </div>
+            <div>
+              {status === 'loading' ? (
+                <div className="">Loading...</div>
+              ) : (
+                <Button onClick={() => signIn('okta')}>Sign in with Okta</Button>
+              )}
+              <noscript>This app requires JavaScript to function. Please enable it to continue.</noscript>
+            </div>
+            <div className="mt-4 text-emphasis-low">Don&apos;t have an account?<br />Contact your organization admin.</div>
+          </Card>
+          <div className="">
+            <Link href="https://asaging.org">
+              <Image src={asaLogo} alt="" width={160} height={120} className="object-contain" />
+            </Link>
+          </div>
+        </main>
+      </div>
+    )
   }
 }
