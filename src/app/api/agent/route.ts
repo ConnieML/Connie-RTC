5f1d0be19ea3c2c55e224bfda1c1b0319d6a617f
Willie Chalmers III <willie@williecubed.me>
2024-02-21 22:29:28 -0800

    Run code formatter

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -1,35 +1,31 @@
-
-import {NextResponse, NextRequest} from 'next/server';
+import { NextResponse, NextRequest } from "next/server";
 
 interface Activity {
-    sid: string
+  sid: string;
 }
 
-export async function POST(
-    request: NextRequest,
-) {
-    const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const authToken = process.env.TWILIO_AUTH_TOKEN;
-    const workspaceSid = process.env.TWILIO_TASKROUTER_WORKSPACE_SID;
-    //Need to update via Okta authentification
-    const workerSid=process.env.TWILIO_WORKER_SID;
-    const client = require('twilio')(accountSid, authToken);
+export async function POST(request: NextRequest) {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const workspaceSid = process.env.TWILIO_TASKROUTER_WORKSPACE_SID;
+  //Need to update via Okta authentification
+  const workerSid = process.env.TWILIO_WORKER_SID;
+  const client = require("twilio")(accountSid, authToken);
 
-    const body = await request.json()
-    const {activitySid} = body;
+  const body = await request.json();
+  const { activitySid } = body;
 
-    try {
-        const worker = await client.taskrouter.v1.workspaces(workspaceSid)
-                                  .workers(workerSid)
-                                  .update({
-                                      activitySid
-                                  });
+  try {
+    const worker = await client.taskrouter.v1
+      .workspaces(workspaceSid)
+      .workers(workerSid)
+      .update({
+        activitySid,
+      });
 
-        console.log(worker.activityName);
-        return NextResponse.json(worker.activityName)
-    } catch (error) {
-        console.error('Failed to update worker:', error);
-        
-    }
+    console.log(worker.activityName);
+    return NextResponse.json(worker.activityName);
+  } catch (error) {
+    console.error("Failed to update worker:", error);
+  }
 }
-
