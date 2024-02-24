65507b146c923e21e01df83a15bcfc2efa2bc8f8
Alex Tsarapkine <alexander.tsarapkine@gmail.com>
2024-02-24 13:19:54 -0500

    Fixed image compilation errors

--- src/components/appbar/Logo.tsx
+++ src/components/appbar/Logo.tsx
@@ -4,12 +4,20 @@ import Image from 'next/image';
 export default function Logo() {
   return (
     <div className="flex flex-row">
-      <Image src="/connie-logo.png" alt="connie logo" className="h-24 sm:h-32" />
+      <Image
+        src="/connie-logo.png"
+        alt="connie logo"
+        className="h-24 sm:h-32"
+        width={128}
+        height={128}
+      />
       <div className="self-center w-0.5 h-6 mx-2 bg-slate-400" />
       <Image
         src="/asa-logo.png"
         alt="asa logo"
         className="h-6 sm:h-8 self-center"
+        width={100}
+        height={32}
       />
     </div>
   );
